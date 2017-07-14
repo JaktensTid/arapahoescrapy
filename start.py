3dc6bf9b5516301f515178c7fc82288138ddf80d
JaktensTid <jaktenstid1@gmail.com>
2017-07-14 10:19:23 +0300

    Running scraper in a loop

--- start.py
+++ start.py
@@ -5,5 +5,6 @@ from scrapy.utils.project import get_project_settings
 while True:
     process = CrawlerProcess(get_project_settings())
     process.crawl('linksspider')
+    print('- - - - - Starting spider')
     process.start()
     gc.collect()
